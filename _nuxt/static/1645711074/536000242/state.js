80a5579337a991e288b35b420940d8d4a6feb5e2
reinout.bossuyt <reinout.bossuyt@gmail.com>
2022-02-24 15:00:08 +0100

    update

---
+++ _nuxt/static/1645711074/536000242/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645711074",layout:"default",error:a,serverRendered:true,routePath:"\u002F536000242",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6c2bb7fe"}}}}(null));
